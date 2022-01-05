fde6fd24fa201445bfca974b567f18078ab81d08
joxiplayer <joxiplayer.id@gmail.com>
2022-01-05 20:45:06 +0700

    first commit

--- data/config/config.js
+++ data/config/config.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyAuloDZVf-JUEalJgUqzUSpO6niyvInfPo",
-    authDomain: "andrep13-id.firebaseapp.com",
-    databaseURL: "https://andrep13-id-default-rtdb.firebaseio.com",
-    projectId: "andrep13-id",
-    storageBucket: "andrep13-id.appspot.com",
-    messagingSenderId: "401123402288",
-    appId: "1:401123402288:web:3c070821ed3ae7668603f5",
-    measurementId: "G-9F9V95DLMR"
+    apiKey: "AIzaSyBfPi9Fh-9sJLE3ViLbvS0OP4pRqf_HSM0",
+    authDomain: "app-chat-andre-13.firebaseapp.com",
+    databaseURL: "https://app-chat-andre-13-default-rtdb.firebaseio.com",
+    projectId: "app-chat-andre-13",
+    storageBucket: "app-chat-andre-13.appspot.com",
+    messagingSenderId: "166899246335",
+    appId: "1:166899246335:web:c6aca9f54d50e16ddc58ca",
+    measurementId: "G-6G5BCNQMFX"
 };
 const app = firebase.initializeApp(firebaseConfig);

3313ef7d4cf99ac0cf16d02d2c2225a88f697c97
joxiplayer <joxiplayer.id@gmail.com>
2022-01-05 20:40:36 +0700

    first commit

---
+++ data/config/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAuloDZVf-JUEalJgUqzUSpO6niyvInfPo",
+    authDomain: "andrep13-id.firebaseapp.com",
+    databaseURL: "https://andrep13-id-default-rtdb.firebaseio.com",
+    projectId: "andrep13-id",
+    storageBucket: "andrep13-id.appspot.com",
+    messagingSenderId: "401123402288",
+    appId: "1:401123402288:web:3c070821ed3ae7668603f5",
+    measurementId: "G-9F9V95DLMR"
+};
+const app = firebase.initializeApp(firebaseConfig);
